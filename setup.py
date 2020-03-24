70b1b6d184d278041394d60cc15b24ea6cb602ac
dalfke <dominik.alfke@mathematik.tu-chemnitz.de>
2020-03-24 13:19:11 +0100

    Added distutils setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'pinvgcn',
+    version = '1.0',
+    description = 'Graph Convolutional Networks with fast Pseudoinverse filters',
+    author = 'Dominik Alfke',
+    author_email = 'dominik.alfke@mathematik.tu-chemnitz.de',
+    url = 'https://github.com/dominikalfke/PinvGCN',
+    packages = ['pinvgcn'],
+    py_modules = [])
