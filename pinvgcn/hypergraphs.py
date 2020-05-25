b3a109a07bb22d1aaa0dcf4c27b188b4eafe04b8
dalfke <dominik.alfke@mathematik.tu-chemnitz.de>
2020-05-25 14:35:56 +0200

    Merged pinvgcn/hypergraphpinv.py into pinvgcn/hypergraphs.py

--- pinvgcn/hypergraphs.py
+++ pinvgcn/hypergraphs.py
@@ -1,10 +1,13 @@
 
 import numpy as np
+import scipy.sparse as sp
 import os
+from warnings import warn
 
 import torch
 from torch_geometric.data import Data, InMemoryDataset, download_url
 
+from .data import setup_spectral_data
 
 
 def load_hypergraph_data(name, dir=None):
@@ -30,6 +33,31 @@ def load_hypergraph_data(name, dir=None):
     
     data.name = name
     return data
+
+
+
+class HypergraphPinv(object):
+    r"""
+    Class in the style of a torch_geometric transform. Replaces given data 
+    by a new data object holding a representation of the hypergraph Laplacian
+    and providing the apply_pinv method. If rank is not None, a low-rank
+    approximation is used. eig_tol is the tolerance for the SVD. eig_threshold
+    determines which eigenvalues are treated as zero."""
+    
+    def __init__(self, rank=None, eig_tol=0, eig_threshold=1e-6):
+        self.rank = rank
+        self.eig_tol = eig_tol
+        self.eig_threshold = eig_threshold
+        
+    def __call__(self, data):
+        inc = normalized_incidence(data)
+    
+        w, U = hypergraph_laplacian_decomposition(inc, num_ev=None if self.rank is None else self.rank+1, tol=self.eig_tol)
+    
+        setup_spectral_data(data, w, U, threshold=self.eig_threshold, max_rank=self.rank)
+    
+        return data
+
 
 
 class UCIHypergraphDataset(InMemoryDataset):
@@ -155,3 +183,39 @@ class CovertypeDataset(UCIHypergraphDataset):
             print(' - Partial incidence shape for classes {}: {}'.format(self.classes, incidence.shape))
             
         self.save_processed(incidence, labels)
+
+
+def normalized_incidence(data):
+    r"""Compute a numpy array holding D^{-1/2} H W^{1/2} D^{-1/2}, where H is 
+    the hypergraph incidence matrix given by data.x, D is the diagonal node
+    degree matrix, B is the diagonal hyperedge degree matrix, and W is the
+    optional diagonal hyperedge weight matrix given by data.hyperedge_weight.
+    """
+    # TODO: check for other sources of incidence in data, e.g., hyperedge_index
+    inc = data.x.cpu().numpy()
+    if 'hyperedge_weight' in data:
+        weights = data.hyperedge_weight.cpu().numpy()
+    else:
+        weights = np.ones(inc.shape[1], dtype=np.float)
+    
+    d = inc @ weights
+    d = 1/np.sqrt(d[:,np.newaxis])
+    b = np.ones(inc.shape[0]) @ inc
+    b = np.sqrt(weights / b)
+    return d * inc * b
+
+def hypergraph_laplacian_decomposition(inc, num_ev=None, tol=0):
+    r"""Return a (partial) eigen decomposition of the hypergraph Laplacian. If
+    num_ev is not None, only that many smallest eigenvalues are computed. The 
+    parameter tol is used for scipy.linalg.svds (if it is called)."""
+    
+    # TODO: get better heuristic of when to choose SVDS over SVD
+    if num_ev is None or num_ev > inc.shape[1]/2:
+        U, sigma, _ = np.linalg.svd(inc, full_matrices=False)
+        if num_ev is not None:
+            U = U[:,:num_ev]
+            sigma = sigma[:num_ev]
+    else:
+        U, sigma, _ = sp.linalg.svds(inc, num_ev, tol=tol)
+        
+    return 1 - sigma.astype(np.float32)**2, U.astype(np.float32)
