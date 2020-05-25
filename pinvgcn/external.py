bce3683714b874f15dacddc53e261c688f652262
dalfke <dominik.alfke@mathematik.tu-chemnitz.de>
2020-05-25 15:11:19 +0200

    Reworked pinvgcn/external.py to follow the new framework

--- pinvgcn/external.py
+++ pinvgcn/external.py
@@ -1,35 +1,21 @@
 
-from .pinvdata import apply_pinv_filter, LowRankPinvData
-
 import numpy as np
 from scipy.io import loadmat
 import os
 
 import torch
+from torch_geometric.data import Data
+
+from .data import setup_spectral_data
 
 
-def load_from_matlab(dataset, filename, rank=None):
-    r"""Load x and y data and Laplacian eigeninformation computed in MATLAB.
-    The file must be located in data/{dataset}/{filename}. If rank is not 
-    None, only that many eigenvalues from the stored decomposition are used.
-    Returns a data object that provides multiplication with the pseudoinverse.
+def load_from_matlab(dataset, filename, max_num_ev=None, sort=True):
+    r"""
+    Load x and y data and Laplacian eigeninformation computed in MATLAB.
+    The file must be located in data/{dataset}/{filename}. If `max_num_ev` is
+    not None, only that many eigenvalues from the stored decomposition are 
+    used. Returns a tuple of eigenvalues, eigenvectors, features, and labels.
     """
-    matfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', dataset, filename)
-    try:
-        matdata = loadmat(matfile)
-        w = matdata['w'].flatten()
-        U = matdata['U']
-        x = matdata['x']
-        y = matdata['y'].flatten()-1
-    except:
-        raise ValueError('Error reading MATLAB file ' + matfile) from None
-    
-    w, U, _ = apply_pinv_filter(w, U, threshold=1e-4, max_rank=rank)
-    
-    return LowRankPinvData(w, U, x=torch.FloatTensor(x), y=torch.LongTensor(y), num_classes=y.max()+1)
-
-
-def load_from_matlab_pure(dataset, filename, max_num_ev=None, sort=True):
     matfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', dataset, filename)
     try:
         matdata = loadmat(matfile)
@@ -51,3 +37,25 @@ def load_from_matlab_pure(dataset, filename, max_num_ev=None, sort=True):
         U = U[:,ind]
     
     return w, U, x, y
+
+
+def setup_spectral_data_from_matlab(dataset, filename, max_num_ev=None, threshold=1e-3):
+    r"""
+    Setup a Data object based on x and y data and Laplacian eigeninformation 
+    computed in MATLAB. The file must be located in data/{dataset}/{filename}. 
+    If `max_num_ev` is not None, only that many eigenvalues from the stored 
+    decomposition are used. Returns a Data object that has been augmented by
+    `setup_spectral_data`.
+    """
+    
+    w, U, x, y = load_from_matlab(dataset, filename, max_num_ev)
+    
+    data = Data(
+        x = torch.tensor(x, dtype=torch.float),
+        y = torch.tensor(y, dtype=torch.long),
+        num_classes = int(y.max())+1,
+        name = dataset)
+    
+    setup_spectral_data(data, w, U, threshold=threshold)
+    
+    return data
